4a9510fa49a8eae8099007f65f5ac5ff7bf8e662
forqan al-kaseb <forqan-01@hotmail.com>
2021-09-14 13:04:10 +0200

    CRUD added to GenreRepository

--- WebApi/Repositories/GenreRepository.cs
+++ WebApi/Repositories/GenreRepository.cs
@@ -10,6 +10,10 @@ namespace WebApi.Repositories
     public interface IGenreRepository
     {
         Task<List<Genre>> GetAll();
+        Task<Genre> GetById(int genreId);
+        Task<Genre> Create(Genre genre);
+        Task<Genre> Update(int genreId, Genre genre);
+        Task<Genre> Delete(int genreId);
     }
 
     public class GenreRepository : IGenreRepository
@@ -24,6 +28,45 @@ namespace WebApi.Repositories
         public async Task<List<Genre>> GetAll()
         {
             return await _context.Genre.ToListAsync();
+        }
+
+        public async Task<Genre> GetById(int genreId)
+        {
+            return await _context.Genre.FirstOrDefaultAsync(g => g.GenreId == genreId);
+        }
+
+        public async Task<Genre> Create(Genre genre)
+        {
+            _context.Genre.Add(genre);
+            await _context.SaveChangesAsync();
+            return genre;
+        }
+
+        public async Task<Genre> Update(int genreId, Genre genre)
+        {
+            Genre updateGenre = await _context.Genre.FirstOrDefaultAsync(a => a.GenreId == genreId);
+
+            if (updateGenre != null)
+            {
+                updateGenre.GenreName = genre.GenreName;
+
+                await _context.SaveChangesAsync();
+            }
+
+            return updateGenre;
+        }
+
+        public async Task<Genre> Delete(int genreId)
+        {
+            Genre genre = await _context.Genre.FirstOrDefaultAsync(g => g.GenreId == genreId);
+
+            if (genre != null)
+            {
+                _context.Genre.Remove(genre);
+                await _context.SaveChangesAsync();
+            }
+
+            return genre;
         }
     }
 }
