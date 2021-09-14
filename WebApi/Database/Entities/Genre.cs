804b39337aae475355e625bd9be28fdb6dbe5c15
forqan al-kaseb <forqan-01@hotmail.com>
2021-09-14 11:29:37 +0200

    WebApiContext added

---
+++ WebApi/Database/Entities/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Database.Entities
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreId { get; set; }
+
+        [Column(TypeName = "nvarchar(32)")]
+        public string GenreName { get; set; }
+    }
+}
---
+++ WebApi/Database/WebApiContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Database.Entities
+{
+    public class WebApiContext : DbContext
+    {
+        public WebApiContext() { }
+        public WebApiContext(DbContextOptions<WebApiContext> options) : base(options) { }
+
+        public DbSet<Genre> Genre { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().HasData(
+                new Genre
+                {
+                    GenreId = 1,
+                    GenreName = "Action"
+                },
+                new Genre
+                {
+                    GenreId = 2,
+                    GenreName = "Comedy"
+                });
+
+
+        }
+    }
+}
