3b753d715605762de00979484349333aaf99cd50
forqan al-kaseb <forqan-01@hotmail.com>
2021-09-16 10:01:35 +0200

    CRUD added to GenreController

--- WebApi/Controllers/GenreController.cs
+++ WebApi/Controllers/GenreController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.DTOs.Requests;
 using WebApi.DTOs.Responses;
 using WebApi.Services;
 
@@ -50,7 +51,8 @@ namespace WebApi.Controllers
 
         [HttpGet("{genreId}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetById([FromRoute] int genreId)
         {
@@ -71,6 +73,73 @@ namespace WebApi.Controllers
 	        }
         }
 
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create([FromBody] NewGenre newGenre)
+        {
+            try
+            {
+                GenreResponse Genre = await _genreService.Create(newGenre);
 
+                if (Genre == null)
+                {
+                    return Problem("Genre was not created, something went wrong");
+                }
+
+                return Ok(Genre);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPut("{genreId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update([FromRoute] int genreId, [FromBody] UpdateGenre updateGenre)
+        {
+            try
+            {
+                GenreResponse Genre = await _genreService.Update(genreId, updateGenre);
+
+                if (Genre == null)
+                {
+                    return Problem("Genre was not updated, something went wrong");
+                }
+
+                return Ok(Genre);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpDelete("{genreId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete([FromRoute] int genreId)
+        {
+            try
+            {
+                bool result = await _genreService.Delete(genreId);
+
+                if (!result)
+                {
+                    return Problem("Genre was not deleted, something went wrong");
+                }
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
     }
 }
