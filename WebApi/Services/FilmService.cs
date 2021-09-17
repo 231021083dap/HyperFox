2c8276783609ff9790ddfc0fe6389b9ab5c37dd8
forqan al-kaseb <forqan-01@hotmail.com>
2021-09-17 12:39:03 +0200

    CRUD added to FilmService

--- WebApi/Services/FilmService.cs
+++ WebApi/Services/FilmService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApi.Database.Entities;
+using WebApi.DTOs.Requests;
 using WebApi.DTOs.Responses;
 using WebApi.Repositories;
 
@@ -11,6 +12,10 @@ namespace WebApi.Services
     public interface IFilmService
     {
         Task<List<FilmResponse>> GetAllFilms();
+        Task<FilmResponse> GetById(int filmId);
+        Task<FilmResponse> Create(NewFilm newFilm);
+        Task<FilmResponse> Update(int filmId, UpdateFilm updateFilm);
+        Task<bool> Delete(int filmId);
     }
 
     public class FilmService : IFilmService
@@ -21,12 +26,11 @@ namespace WebApi.Services
         {
             _filmRepository = filmRepository;
         }
-
         public async Task<List<FilmResponse>> GetAllFilms()
         {
-            List<Film> Films = await _filmRepository.GetAll();
+            List<Film> films = await _filmRepository.GetAll();
 
-            return Films.Select(f => new FilmResponse
+            return films == null ? null : films.Select(f => new FilmResponse
             {
                 FilmId = f.FilmId,
                 FilmName = f.FilmName,
@@ -37,6 +41,85 @@ namespace WebApi.Services
                 Stock = f.Stock,
                 Image = f.Image
             }).ToList();
+        }
+
+        public async Task<FilmResponse> GetById(int filmId)
+        {
+            Film film = await _filmRepository.GetById(filmId);
+
+            return film == null ? null : new FilmResponse
+            {
+                FilmId = film.FilmId,
+                FilmName = film.FilmName,
+                ReleaseDate = film.ReleaseDate,
+                RuntimeInMin = film.RuntimeInMin,
+                Description = film.Description,
+                Price = film.Price,
+                Stock = film.Stock,
+                Image = film.Image
+            };
+        }
+
+        public async Task<FilmResponse> Create(NewFilm newFilm)
+        {
+            Film film = new Film
+            {
+                FilmName = newFilm.FilmName,
+                ReleaseDate = newFilm.ReleaseDate,
+                RuntimeInMin = newFilm.RuntimeInMin,
+                Description = newFilm.Description,
+                Price = newFilm.Price,
+                Stock = newFilm.Stock,
+                Image = newFilm.Image
+            };
+
+            film = await _filmRepository.Create(film);
+
+            return film == null ? null : new FilmResponse
+            {
+                FilmId = film.FilmId,
+                FilmName = film.FilmName,
+                ReleaseDate = film.ReleaseDate,
+                RuntimeInMin = film.RuntimeInMin,
+                Description = film.Description,
+                Price = film.Price,
+                Stock = film.Stock,
+                Image = film.Image
+            };
+        }
+
+        public async Task<FilmResponse> Update(int filmId, UpdateFilm updateFilm)
+        {
+            Film film = new Film
+            {
+                FilmName = updateFilm.FilmName,
+                ReleaseDate = updateFilm.ReleaseDate,
+                RuntimeInMin = updateFilm.RuntimeInMin,
+                Description = updateFilm.Description,
+                Price = updateFilm.Price,
+                Stock = updateFilm.Stock,
+                Image = updateFilm.Image
+            };
+
+            film = await _filmRepository.Update(filmId, film);
+
+            return film == null ? null : new FilmResponse
+            {
+                FilmId = film.FilmId,
+                FilmName = film.FilmName,
+                ReleaseDate = film.ReleaseDate,
+                RuntimeInMin = film.RuntimeInMin,
+                Description = film.Description,
+                Price = film.Price,
+                Stock = film.Stock,
+                Image = film.Image
+            };
+        }
+
+        public async Task<bool> Delete(int filmId)
+        {
+            var result = await _filmRepository.Delete(filmId);
+            return true;
         }
     }
 }
