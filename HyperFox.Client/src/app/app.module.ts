fe9fae2f66704bc6b93ff0f9b91b7f0062d26cb1
forqan al-kaseb <forqan-01@hotmail.com>
2021-09-24 12:34:23 +0200

    info added

--- HyperFox.Client/src/app/app.module.ts
+++ HyperFox.Client/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SlideComponent } from './components/slide/slide.component';
 import { InfoComponent } from './components/info/info.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SlideComponent,
-    InfoComponent
+    InfoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
