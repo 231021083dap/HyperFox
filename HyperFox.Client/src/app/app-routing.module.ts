17bb4ce28a1c209c50440e15d46d60385f9e8ac2
forqan al-kaseb <forqan-01@hotmail.com>
2021-09-27 09:08:59 +0200

    film added

--- HyperFox.Client/src/app/app-routing.module.ts
+++ HyperFox.Client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmComponent } from './components/admin/film/film.component';
 import { HomeComponent } from './components/home/home.component';
 import { InfoComponent } from './components/info/info.component';
 import { LoginComponent } from './components/login/login.component';
@@ -7,7 +8,8 @@ import { LoginComponent } from './components/login/login.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'info', component: InfoComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'admin/film', component: FilmComponent}
 ];
 
 @NgModule({
