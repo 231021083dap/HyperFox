b9dac8107df151050fa68dca7fc38ba004504e1f
Luan Faber <krytanx@gmail.com>
2021-09-28 14:00:39 +0200

    -- Login

--- HyperFox.Client/src/app/app.component.ts
+++ HyperFox.Client/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import { User } from './models';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentUser: User = {id: 0, email: '', username:''};
   title = 'HyperFox-Client';
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ){
+    //
+    this.authenticationService.
+
+  }
 }
---
+++ HyperFox.Client/src/app/models.ts
@@ -0,0 +1,28 @@
+export interface Author {
+    id: number,
+    firstName: string,
+    lastName: string,
+    middleName: string
+  }
+  
+  export interface Book {
+    id: number;
+    title: string;
+    pages: number;
+    authorId: number;
+    author?: Author;
+  }
+  
+  export interface User {
+    id: number;
+    username: string;
+    email: string;
+    role?: Role;
+    token?: string;
+  }
+  
+  export enum Role {
+    User = 'User',
+    Admin = 'Admin'
+  }
+  
